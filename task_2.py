41c3a57dc689f16d7b9f3e15648ad60c3dcec01a
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-07-02 12:28:45 +0300

    Update task_2.py
    Rewrite code because task changed.

--- task_2.py
+++ task_2.py
@@ -1,46 +1,75 @@
-import os.path
+class IncorrectVinNumber(Exception):
 
-
-class UnAvailableFileName(Exception):
-  def __init__(self, message, info):
+  def __init__(self, message):
     self.message = message
-    self.info = info
 
 
-class NotNatureNumber(Exception):
-  def __init__(self, message, info):
+class IncorrectCarNumbers(Exception):
+
+  def __init__(self, message):
     self.message = message
-    self.info = info
 
 
-def test_func(args):
-  for key, value in args.items():
-    if key == 'file_name':
-      if os.path.isfile(value):
-        with open(value, 'r') as file:
-          print(file.read())
-      else:
-        raise UnAvailableFileName(f'File with name {value} is unavaililable', f'{key}:{value}')
+class Car():
+
+  def __init__(self, model, vin, numbers):
+    self.__vin_start_count = 1000000
+    self.__vin_end_count = 9999999
+    self.__numbers_len = 6
+    self.model = model
+    if self.__is_valid_vin(vin):
+      self.__vin = vin
+    if self.__is_valid_numbers(numbers):
+      self.__numbers = numbers
+
+  def __is_valid_vin(self, vin_number):
+    if not isinstance(vin_number, int):
+      raise IncorrectVinNumber(
+          f'Некорректный тип vin номер. Должен быть "int", получен {type(vin_number)}'
+      )
+    elif self.__vin_start_count > vin_number or vin_number > self.__vin_end_count:
+      raise IncorrectVinNumber(
+          f'Неверный диапазон для vin номера. Должен быть в пределах от {self.__vin_start_count} до {self.__vin_end_count}. vin = {vin_number}'
+      )
     else:
-      if isinstance(value, int):
-        if value < 0:
-          raise NotNatureNumber(f'Number {value} is not nature number', f'error because number have "{str(value)[0]}"')
-        else:
-          print(f'Number {round(value, 0)} is nature number')          
+      return True
 
-def main():
-  try:
-    test_func({'file_name':'test.txt'})
-  except UnAvailableFileName as e:
-    print(e.message)
-    print(e.info)
+  def __is_valid_numbers(self, numbers):
+    if not isinstance(numbers, str):
+      raise IncorrectCarNumbers(
+          f'Некорректный тип данных для номеров. Должен быть "str", получен {type(numbers)}'
+      )
+    elif len(numbers) != self.__numbers_len:
+      raise IncorrectCarNumbers(
+          f'Неверная длина номера. Должна быть ровно {self.__numbers_len} символов. Получено {len(numbers)} символов.'
+      )
+    else:
+      return True
 
-  try:
-    test_func({'value':-2})
-  except NotNatureNumber as e:
-    print(e.message)
-    print(e.info)
-  finally:
-    print('Congratulation. We succesfully finished practice.')
 
-main()
+try:
+  first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{first.model} успешно создан')
+
+try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
+
+try:
+  third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{third.model} успешно создан')
